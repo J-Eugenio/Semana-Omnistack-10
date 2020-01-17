9f2d37e0d8b2e1bc82aa1326ff9ce95645a2ce78
Eugenio <eugeniopb4@gmail.com>
2020-01-16 22:21:04 -0300

    Estilo da pagina principal + CSS's

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import './Global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da app
+// Propriedade: Informações que um componente pai passa para o com filho
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div id="app">
+     <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <div className="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input name="github_username" id="username_github"></input>
+        </div>
+
+        <div class="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs"></input>
+        </div>
+
+        <div className="input-group">
+          <div class="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude"></input>
+          </div>
+          <div class="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude"></input>
+          </div>
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
+     </aside>
+     <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37482325?s=460&v=4" alt="José Eugênio"/>
+              <div className="user-info">
+                <strong>José Eugênio</strong>
+                <span>C++, ReactJS, ReactNative</span>
+              </div>
+            </header>
+            <p>Graduando em Análise e Desenvolvimento de sistemas pela Faculdade vale do salgado, participo de projetos como a Fabrica de Software que visa a integração de alunos ao mercado, desenvolvendo sistemas para clientes reais. grande experiencia em Segurança da Informação estudo a muito tempo essa área, alem de programação ser meu ponto mais forte. sempre foi o meu maior interesse desenvolver sistemas diversos para as mais diversas situações. </p>
+            <a href="https://github.com/J-Eugenio">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37482325?s=460&v=4" alt="José Eugênio"/>
+              <div className="user-info">
+                <strong>José Eugênio</strong>
+                <span>C++, ReactJS, ReactNative</span>
+              </div>
+            </header>
+            <p>Graduando em Análise e Desenvolvimento de sistemas pela Faculdade vale do salgado, participo de projetos como a Fabrica de Software que visa a integração de alunos ao mercado, desenvolvendo sistemas para clientes reais. grande experiencia em Segurança da Informação estudo a muito tempo essa área, alem de programação ser meu ponto mais forte. sempre foi o meu maior interesse desenvolver sistemas diversos para as mais diversas situações. </p>
+            <a href="https://github.com/J-Eugenio">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37482325?s=460&v=4" alt="José Eugênio"/>
+              <div className="user-info">
+                <strong>José Eugênio</strong>
+                <span>C++, ReactJS, ReactNative</span>
+              </div>
+            </header>
+            <p>Graduando em Análise e Desenvolvimento de sistemas pela Faculdade vale do salgado, participo de projetos como a Fabrica de Software que visa a integração de alunos ao mercado, desenvolvendo sistemas para clientes reais. grande experiencia em Segurança da Informação estudo a muito tempo essa área, alem de programação ser meu ponto mais forte. sempre foi o meu maior interesse desenvolver sistemas diversos para as mais diversas situações. </p>
+            <a href="https://github.com/J-Eugenio">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/37482325?s=460&v=4" alt="José Eugênio"/>
+              <div className="user-info">
+                <strong>José Eugênio</strong>
+                <span>C++, ReactJS, ReactNative</span>
+              </div>
+            </header>
+            <p>Graduando em Análise e Desenvolvimento de sistemas pela Faculdade vale do salgado, participo de projetos como a Fabrica de Software que visa a integração de alunos ao mercado, desenvolvendo sistemas para clientes reais. grande experiencia em Segurança da Informação estudo a muito tempo essa área, alem de programação ser meu ponto mais forte. sempre foi o meu maior interesse desenvolver sistemas diversos para as mais diversas situações. </p>
+            <a href="https://github.com/J-Eugenio">Acessar perfil no Github</a>
+          </li>
+        </ul>
+     </main>
+   </div>
   );
 }
 
